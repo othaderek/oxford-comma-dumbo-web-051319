c22f744d39c920c862dd4b7ac765f7c10b2b95d3
Otha D Hernandez <otha.hernandez@gmail.com>
2019-04-24 23:37:41 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,12 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array.join
+    elsif array.length == 2
+    array.join(" and ")
+    elsif array.length >= 3
+    lastword = array.pop
+    lastword.prepend("and ")
+    array << lastword
+    array.join(", ")
+  end
 end
